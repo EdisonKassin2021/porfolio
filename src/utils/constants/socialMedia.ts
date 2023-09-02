b2b278b1892d8690784b66612a9b3ba21c9ac65b
EdisonKassin2021 <edison.kassin.etu@esmt.sn>
2023-09-02 19:35:45 +0000

    add: add social media url

--- src/utils/constants/socialMedia.ts
+++ src/utils/constants/socialMedia.ts
@@ -1,4 +1,4 @@
-export const LinkedIn = "";
+export const LinkedIn = "www.linkedin.com/in/edison-kassin";
 export const Twitter = "";
-export const Github = "";
-export const Gmail = "";
+export const Github = "https://github.com/EdisonKassin2021";
+export const Gmail = "kassinsaturninedison@gmail.com";
