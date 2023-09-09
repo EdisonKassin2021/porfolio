72a7844306c4379c507a1d54d39371e40496c4d5
EdisonKassin2021 <edison.kassin.etu@esmt.sn>
2023-09-09 19:45:12 +0000

    restore token

--- vite.config.ts
+++ vite.config.ts
@@ -4,27 +4,27 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ["fsevents"],
-      output: {
-        plugins: [
-          // replace({
-          //   "process.env.VITE_GITHUB_TOKEN": JSON.stringify(
-          //     process.env.VITE_GITHUB_TOKEN
-          //   ),
-          //   "process.env.VITE_GITHUB_USERNAME": JSON.stringify(
-          //     process.env.VITE_GITHUB_USERNAME
-          //   ),
-          //   "process.env.VITE_ENV": JSON.stringify(process.env.VITE_ENV),
-          // }),
-        ],
-        manualChunks: {
-          "vite-deploy": ["vite"],
-        },
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: ["fsevents"],
+  //     output: {
+  //       plugins: [
+  //         // replace({
+  //         //   "process.env.VITE_GITHUB_TOKEN": JSON.stringify(
+  //         //     process.env.VITE_GITHUB_TOKEN
+  //         //   ),
+  //         //   "process.env.VITE_GITHUB_USERNAME": JSON.stringify(
+  //         //     process.env.VITE_GITHUB_USERNAME
+  //         //   ),
+  //         //   "process.env.VITE_ENV": JSON.stringify(process.env.VITE_ENV),
+  //         // }),
+  //       ],
+  //       manualChunks: {
+  //         "vite-deploy": ["vite"],
+  //       },
+  //     },
+  //   },
+  // },
   base: "/portfolio/",
   plugins: [react()],
 });
