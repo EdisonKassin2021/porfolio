282419efabeba32347bde3eb3354a8654c61db23
EdisonKassin2021 <edison.kassin.etu@esmt.sn>
2023-09-09 19:35:17 +0000

    restore token

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import replace from "@rollup/plugin-replace";
+// import replace from "@rollup/plugin-replace";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,15 +9,15 @@ export default defineConfig({
       external: ["fsevents"],
       output: {
         plugins: [
-          replace({
-            "process.env.VITE_GITHUB_TOKEN": JSON.stringify(
-              process.env.VITE_GITHUB_TOKEN
-            ),
-            "process.env.VITE_GITHUB_USERNAME": JSON.stringify(
-              process.env.VITE_GITHUB_USERNAME
-            ),
-            "process.env.VITE_ENV": JSON.stringify(process.env.VITE_ENV),
-          }),
+          // replace({
+          //   "process.env.VITE_GITHUB_TOKEN": JSON.stringify(
+          //     process.env.VITE_GITHUB_TOKEN
+          //   ),
+          //   "process.env.VITE_GITHUB_USERNAME": JSON.stringify(
+          //     process.env.VITE_GITHUB_USERNAME
+          //   ),
+          //   "process.env.VITE_ENV": JSON.stringify(process.env.VITE_ENV),
+          // }),
         ],
         manualChunks: {
           "vite-deploy": ["vite"],
