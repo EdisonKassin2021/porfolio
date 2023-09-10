1e6f5726ad742c8cbc335de7c70ad42feceaeac9
EdisonKassin2021 <edison.kassin.etu@esmt.sn>
2023-09-10 19:47:32 +0000

    add: insert formation

--- src/configs/supabase.ts
+++ src/configs/supabase.ts
@@ -15,9 +15,10 @@ const getSupabaseAnonKey = () => {
   return import.meta.env.SUPABASE_ANON;
 };
 
-const url = (getSupabaseUrl() ??
-  "https://hmexluljreaekzdfulwg.supabase.co") as string;
-export const anon = getSupabaseAnonKey() ?? "";
+const url = getSupabaseUrl() ?? "https://hmexluljreaekzdfulwg.supabase.co";
+export const anon =
+  getSupabaseAnonKey() ??
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtZXhsdWxqcmVhZWt6ZGZ1bHdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM3NDQ4NjUsImV4cCI6MjAwOTMyMDg2NX0.Wf9uHWgyIODEDxRaE864U6lf8tTMVIioGewlT4CmXK0";
 
 const supabase = createClient(url, anon);
 export default supabase;
