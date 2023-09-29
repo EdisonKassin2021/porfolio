892bce9befbf87324135f5f4b9d27a40071eaf1e
EdisonKassin2021 <edison.kassin.etu@esmt.sn>
2023-09-29 09:40:34 +0000

    fix github error

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import RouteGroups from "./routes/Routes.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./redux/app/store.ts";
 import { Provider } from "react-redux";
+import { BASE_ROUTE } from "./routes/constants.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <BrowserRouter>
+    {/* Fix github error 404: https://youtu.be/fuGu-Ponjf8 */}
+    <BrowserRouter basename={BASE_ROUTE}>
       <RouteGroups />
     </BrowserRouter>
   </Provider>
